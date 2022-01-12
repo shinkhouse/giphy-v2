beb361daf6457794286c5534f3b90dd11acbcbbc
Samuel Hinkhouse <shinkhouse@nfsmith.com>
2022-01-12 14:25:56 -0600

    update to angular 11

--- src/app/core/components/search-field/search-field.component.spec.ts
+++ src/app/core/components/search-field/search-field.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { SearchFieldComponent } from './search-field.component';
 
@@ -6,7 +6,7 @@ describe('SearchFieldComponent', () => {
   let component: SearchFieldComponent;
   let fixture: ComponentFixture<SearchFieldComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ SearchFieldComponent ]
     })
